df9ba8602eab0933345f2377800da18bd0029844
Bjverde <bjverde@yahoo.com.br>
2018-11-08 01:23:33 -0200

    retirando metodos não usados

--- www/cargaCorretagem/classes/Documento.class.php
+++ www/cargaCorretagem/classes/Documento.class.php
@@ -4,44 +4,6 @@ class Documento {
 
 	public function __construct(){
 	}
-	//--------------------------------------------------------------------------------
-	public static function selectById( $id ){
-	    $mongoBD = new DocumentoDAO();
-	    $result = $mongoBD->selectById( $id );
-	    $result = self::trataDados($result);
-		return $result;
-	}
-	//--------------------------------------------------------------------------------
-	//--------------------------------------------------------------------------------	
-	/**
-	 * 
-	 * @param array $arrayFilter      - campos de pesquisa
-	 * @param array $arraySort        - ordenação
-	 * @param number $skip            - paginação
-	 * @param number $limit           - qtd de registro
-	 * @param boolean $resultFormDin  - resulta para apresentar no formDin ou não
-	 * @return NULL|array
-	 */
-	public static function selectAll( $arraySort=null ,$arrayFilter=null ,$limit=null ,$skip=null ,$resultFormDin=true  ){
-	    if( !empty($arrayFilter) ){
-	       $arrayFilter  = self::filtrosBusca( $arrayFilter );
-	    }
-	    
-		$arraySort = array( 'sort' => array('dt'=>-1) );  //Faz ordenação DECRESCENTE pela data de publicacao / decisão
-		
-		if( empty($limit) ){
-		    $limit  = 1000 ; //Limita os resultados aos 1.000 registros
-		}
-	    
-	    //https://code.tutsplus.com/tutorials/full-text-search-in-mongodb--cms-24835
-	    //https://dzone.com/articles/find-mongo-document-by-id-using-the-php-library
-	    $mongoBD = new DocumentoDAO();
-	    $result = $mongoBD->selectAll($arraySort ,$arrayFilter ,$limit ,$skip ,$resultFormDin);
-	    if($resultFormDin){
-	        $result = self::trataDados($result);
-	    }
-		return $result;
-	}
 	//--------------------------------------------------------------------------------	
 	/***
 	 * Trata o arquivo temp movendo para a pasta definitiva.
@@ -64,25 +26,17 @@ class Documento {
 	    //As duas linhas abaixo serão utilizadas para gravar os arquivos tambem no file system
 	    $arquivo->setDiretorioRaiz(DIR_ARQ_DIARIO);
 	    $arquivo->moveArquivoParaDestino();
-		$objVo->setLink($link);
-	    
+		$objVo->setLink($link);	    
 	    
 	    $arquivoTMP['arquivo_name'] = $arquivo->getNome();
 		$arquivoTMP['arquivo_name_apache'] = $arquivo->getNomeCanonico();
-		$arquivoTMP['arquivo_name'] = 't.pdf';
-		$arquivoTMP['arquivo_name_apache'] = $arquivoTMP['arquivo_temp_name'];
 	    $objVo->setArquivo($arquivoTMP);
-	    
-		$inteiroTeor = pdfParser::parseFile($arquivoTMP['arquivo_temp_name']);
-		//$inteiroTeor = pdfParser::parseFile('../NotaCorretagem_30487_20181008.pdf');
+		
+		$arquivo_caminho_completo = $arquivo->getNomeCanonico();
+		$inteiroTeor = pdfParser::parseFile( $arquivo_caminho_completo );
 	    $objVo->setTrecho($inteiroTeor);
 	    
 	    return $objVo;
-	}
-	
-	public static function getAno( $data ){
-	    $data = explode('/', $data);
-	    return $data[2];
 	}
 	//--------------------------------------------------------------------------------
 	public static function save( DocumentoVO $objVo ){
@@ -92,24 +46,6 @@ class Documento {
 		
 		$result = 1;
 		return $result;
-	}
-	//--------------------------------------------------------------------------------
-	public static function delete( $id ){
-	    $result = self::selectById($id);	    
-	    $hasArray = ArrayHelper::has('PDF', $result);
-	    if($hasArray){
-	        $oid=get_object_vars($result['PDF'][0]);
-	        File::delete($oid['oid']);
-	    }
-	    $mongoBD = new DocumentoDAO();
-	    $result = $mongoBD->delete( $id );
-		return $result;
-	}
-	//--------------------------------------------------------------------------------
-	public static function openFile( $idFile ,$arquivoTmp ){
-	    $mongoBD = new DocumentoDAO();
-	    $result = $mongoBD->openFile( $idFile ,$arquivoTmp );
-	    return $result;
 	}
 }
 ?>
