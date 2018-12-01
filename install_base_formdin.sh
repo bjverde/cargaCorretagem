7df8ab9beec07b8e20f89f1be826991a670cf01f
Bjverde <bjverde@yahoo.com.br>
2018-12-01 13:09:55 -0200

    alterando script para pegar o formDin

--- install_base_formdin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "teste"
-#git clone https://github.com/bjverde/formDin.git
-# cd formDin;
-# mv -v base/ ../;
-# cd ..;
-# rm -vfr formDin
---
+++ www/install_base_formdin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#STYLE_COLOR
+RED='\033[0;31m';
+LIGHT_GREEN='\e[1;32m';
+NC='\033[0m' # No Color
+
+echo -e "${LIGHT_GREEN} Download FormDin by Git Clone ${NC}"
+echo "teste"
+#git clone https://github.com/bjverde/formDin.git
+# cd formDin;
+# mv -v base/ ../;
+# cd ..;
+# rm -vfr formDin
